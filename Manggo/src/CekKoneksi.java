01af764ce5549ce5757e5f3349b76272221b0635
Mahesta Riyadi Putranto <adi504097@gmail.com>
2023-05-21 00:08:52 +0700

    Connect to database has been successfully

--- Manggo/src/CekKoneksi.java
+++ Manggo/src/CekKoneksi.java
@@ -1,14 +1,59 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 public class CekKoneksi {
 
-    public static void main(String[] args) {
-
-        String url = ""
-
+    static Connection conn = null;
+    static final String url = "jdbc:mysql://localhost:3306/data_mahasiswa" ;
+    static final String username = "root" ;
+    static final String pass = "" ;
+    
+    
+    public static void main (String[]args){
+        
+            
+    try {
+    
+    Class.forName("com.mysql.cj.jdbc.Driver");
+    
+    conn = DriverManager.getConnection(url, username, pass);
+    
+    JOptionPane.showMessageDialog(null, "Koneksi database berhasil!");
+    
+  }catch (SQLException e) {
+      
+      JOptionPane.showMessageDialog(null, "Koneksi database gagal!");
+       e.printStackTrace();
+      
+  }catch (ClassNotFoundException e) {
+      
+      JOptionPane.showConfirmDialog(null, "Driver tidak ditemukan!");
+       e.printStackTrace();
+      
+  } finally {
+        
         try {
-
-            Class.forName(jdbc_driver)
+            
+            if(conn == null) {
+                
+                conn.close();
+                
+            }
+            
+        }catch (SQLException e) {
+             e.printStackTrace();
         }
-
+        
+        
     }
+            
+    
+    }
+    
+ 
 
 }
