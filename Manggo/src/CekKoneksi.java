d605a3b3ec4c64564c64a5084cd47aeaf77f1c27
Mahesta Riyadi Putranto <adi504097@gmail.com>
2023-05-20 23:30:43 +0700

    refresh

--- Manggo/src/CekKoneksi.java
+++ Manggo/src/CekKoneksi.java
@@ -2,6 +2,13 @@ public class CekKoneksi {
 
     public static void main(String[] args) {
 
+        String url = ""
+
+        try {
+
+            Class.forName(jdbc_driver)
+        }
+
     }
 
 }
